5acb70a69594623f0617eec3f2e081119f2d1bc8
MuhammadTouqeerAhmad <muhammadtouqeerahmad40@gmail.com>
2026-08-10 23:11:45 +0500

    An issue inside the AppContext is resolved

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -6,7 +6,10 @@ import { RouterContextProvider } from 'react-router-dom'
 
 export const AppContext = createContext();
 const BACKEND_URL = import.meta.env.VITE_API_BASE_URL
-
+if (!BACKEND_URL) {
+    throw new Error("Api_baseUrl is not configured correctly");
+    
+}
 const AppContextProvider = (props) => {
     const value ={
         BACKEND_URL
